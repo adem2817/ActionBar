abdf4463dd8c8f148e634a5e1415a2267269033b
Adem YILMAZ <adem2817@gmail.com>
2018-10-04 22:41:38 +0300

    Project Added

---
+++ ActionBar/app/src/main/java/com/example/lab08/tema/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.lab08.tema;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
